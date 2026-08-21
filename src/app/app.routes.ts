2316061dca8a855cbe62c456d0f03bc2c6384ea2
Anna Louro <annab.louro@gmail.com>
2026-08-20 23:01:40 -0300

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { Contato } from './contato/contato';
-
-export const routes: Routes = [
-  { path: '', component: Contato },
-  { path: 'contato', component: Contato }
-];
--- src/app/contato/contato.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-contato',
-  styleUrl: './contato.css',
-  templateUrl: './contato.html',
-})
-export class Contato {}
